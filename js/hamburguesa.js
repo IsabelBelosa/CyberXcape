b1eb329744c588a0e6381247cf489e3bca6d0723
Isabel <115543044+IsabelBelosa@users.noreply.github.com>
2025-03-31 20:15:27 +0200

    borrrado completo

--- js/hamburguesa.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.getElementById("menu-icon");
-    const navList = document.getElementById("nav-list");
-
-    menuIcon.addEventListener("click", function () {
-        navList.classList.toggle("open"); // Activa/desactiva la clase 'open'
-        menuIcon.innerHTML = navList.classList.contains("open") ? "✖" : "&#9776;"; // Cambia el icono
-    });
-});
